d68ebd0a3fe0c7def405ab03298e1ac11addff42
hongya <hong7ya@gmail.com>
2025-01-14 11:43:48 +0900

    chore: tailwindcss 설정

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "mainSkyblue": "#6CAFD9",
+        "subGray": "#333",
+      }
+    },
+  },
+  plugins: [],
+}
+
