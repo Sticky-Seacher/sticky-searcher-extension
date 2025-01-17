3a1b9ed6ff2a1e5bc2586ae97a957ac62072a93d
Under <52349132+josuk0212@users.noreply.github.com>
2025-01-17 12:31:09 +0900

    feat: 사이드 패널 구축 (#20)
    * feat: 사이드 패널 구축 및 활성화 단축키 지정
    
    * feat: 번들러 추가 설정 및 description text 추출
    
    * fix: 번들 실패 오류 해결
    
    * refactor: 불필요한 코드 제거 및 문법 변경
    
    ---------
    
    Co-authored-by: JSLee <mac@macui-MacBookPro.local>

--- public/service-worker.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-undef */
-
-// Allows users to open the side panel by clicking on the action toolbar icon
-chrome.sidePanel
-  .setPanelBehavior({ openPanelOnActionClick: true })
-  .catch((error) => console.error(error));
