d7e429dbbe848cedf775661c3832087e34efb392
hongya <hong7ya@gmail.com>
2025-01-17 09:20:52 +0900

    feat: 하이라이팅 적용 로직 구현 (#17)
    * feat: 멀티 키워드 하이라이트
    
    * fix: tag 속성키값에 영향없이 content의 text만 하이라이팅되도록 수정
    
    * fix: 멀티 키워드일 경우 노드가 삭제되지 않는 이슈 해결
    
    * style: 주석 삭제
    
    * refactor: 명시적인 변수명
    
    * refactor: 명시적인 콜백명

---
+++ src/highlight.js
@@ -0,0 +1,72 @@
+const READABLE_RGB_RANGE = 150;
+
+export function makeRandomColor(rgbRangeEnd) {
+  const r = Math.floor(Math.random() * rgbRangeEnd);
+  const g = Math.floor(Math.random() * rgbRangeEnd);
+  const b = Math.floor(Math.random() * rgbRangeEnd);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function getColors(numberOfColorsNeeded) {
+  const memory = [];
+
+  return Array.from(Array(numberOfColorsNeeded), () => {
+    let color = makeRandomColor(READABLE_RGB_RANGE);
+
+    while (memory.includes(color)) {
+      color = makeRandomColor(READABLE_RGB_RANGE);
+    }
+
+    memory.push(color);
+    return color;
+  });
+}
+
+function setHighlight(keyword, targetElement, color) {
+  const textNodeIterator = document.createNodeIterator(
+    targetElement,
+    NodeFilter.SHOW_TEXT
+  );
+
+  for (
+    let currentTextNode = textNodeIterator.nextNode();
+    currentTextNode;
+    currentTextNode = textNodeIterator.nextNode()
+  ) {
+    const parentElement = currentTextNode.parentElement;
+    const text = currentTextNode.textContent;
+
+    const excludedList = text.split(keyword);
+
+    excludedList.forEach((stringFragment, index) => {
+      const notKeywordText = document.createTextNode(stringFragment);
+      parentElement.insertBefore(notKeywordText, currentTextNode);
+
+      if (index !== excludedList.length - 1) {
+        const highlightedSpan = document.createElement("span");
+        highlightedSpan.textContent = keyword;
+        highlightedSpan.style = `background:${color}`;
+        parentElement.insertBefore(highlightedSpan, currentTextNode);
+      }
+    });
+
+    parentElement.removeChild(currentTextNode);
+  }
+}
+
+export function highlightKeywords(keywords, body, selectorAllOfKeywordParent) {
+  const targetElements = [];
+  const colors = getColors(keywords.length);
+
+  for (const keyword of keywords) {
+    targetElements.push(...selectorAllOfKeywordParent(body, keyword));
+  }
+
+  const uniqueElements = new Set(targetElements);
+
+  for (const element of uniqueElements) {
+    for (let i = 0; i < keywords.length; i += 1) {
+      setHighlight(keywords[i], element, colors[i]);
+    }
+  }
+}
