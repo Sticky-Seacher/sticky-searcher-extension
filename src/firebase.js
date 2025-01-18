5e7e356d4757222afd67aa71aa330c6a438c1e29
JSLee <mac@macui-MacBookPro.local>
2025-01-18 09:56:07 +0900

    feat: firebase 연동

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_REACT_APP_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENTID,
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { analytics };
