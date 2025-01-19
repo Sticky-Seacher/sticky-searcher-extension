dd1966e979d7da61f0603e09f740f4edd482412e
Under <52349132+josuk0212@users.noreply.github.com>
2025-01-19 13:49:44 +0900

    feat: DB 구축 (#23)
    * feat: firebase 연동
    
    * feat: firebase 연동 및 데이터 하위 컬렉션 구조화
    
    * feat: CRUD 추가
    
    ---------
    
    Co-authored-by: JSLee <mac@macui-MacBookPro.local>

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_REACT_APP_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENTID,
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db, analytics };
