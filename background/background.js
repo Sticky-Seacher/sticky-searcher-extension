9cb0b8a957e881055eb765c5907e488270efd673
hongya <hong7ya@gmail.com>
2025-01-21 19:08:36 +0900

    feat: 스크롤 구현 (#29)
    * feat: 구글 검색 페이지에 있는 링크 클릭 시, 열린 페이지의 description 영역으로 스크롤
    
    * feat: keyword 전체 토글 버튼 클릭 시에 하이라이트 적용
    
    * feat: 사이드패널 내부의 화살표 버튼을 클릭했을 때, 다음/이전 키워드 위치로 스크롤
    
    * feat: 유동적인 키워드 대응
    
    * fix: 구글 검색 업데이트 후 링크타고 페이지 이동 시 스크롤이 작동하지 않는 버그 수정
    
    * fix: 스크롤 시 음수이거나 요소 갯수를 초과하는 경우를 막는 경계값 설정
    
    * feat: 현재 스크롤 위치의 키워드만 배경색 변경
    
    * refactor: 명시적인 변수명

--- background/background.js
+++ background/background.js
@@ -1,4 +1,13 @@
 import iconUrl from "../public/sticky-seacher-logo.png";
+import { convertToLinkMap } from "./convertToLinkMap";
+import { createRules } from "./createRules";
+
+chrome.runtime.onInstalled.addListener(async function () {
+  const existingRules = await chrome.declarativeNetRequest.getDynamicRules();
+  chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: existingRules.map((rule) => rule.id),
+  });
+});
 
 chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
@@ -22,9 +31,13 @@ chrome.webNavigation.onCompleted.addListener((details) => {
   if (isGoogleSearch) {
     const tabId = details.tabId;
 
-    async function sendMessageToActiveTab(message) {
+    async function sendMessageToActiveTab(message, callbback = () => {}) {
       const response = await chrome.tabs.sendMessage(tabId, message);
 
+      callbback(response);
+    }
+
+    sendMessageToActiveTab("give-me-linkMap", (response) => {
       chrome.storage.local.set({ [`${tabId}`]: response }).then(() => {
         chrome.notifications.create({
           type: "basic",
@@ -42,8 +55,30 @@ chrome.webNavigation.onCompleted.addListener((details) => {
           message: data[tabId],
         });
       });
-    }
-
-    sendMessageToActiveTab("give-me-linkMap");
+    });
   }
+});
+
+chrome.storage.onChanged.addListener((changes, area) => {
+  if (area === "local") {
+    chrome.declarativeNetRequest.getDynamicRules().then((existingRules) => {
+      const newRules = [];
+
+      for (let { newValue } of Object.values(changes)) {
+        if (newValue) {
+          const newLinkMaps = convertToLinkMap(newValue);
+          newRules.push(...createRules(newLinkMaps, 1));
+        }
+      }
+
+      chrome.declarativeNetRequest.updateDynamicRules({
+        removeRuleIds: existingRules.map((rule) => rule.id),
+        addRules: newRules,
+      });
+    });
+  }
+});
+
+chrome.tabs.onRemoved.addListener((tabId) => {
+  chrome.storage.local.remove(`${tabId}`);
 });
--- src/components/search-section/SearchSectionInput.jsx
+++ src/components/search-section/SearchSectionInput.jsx
@@ -1,4 +1,42 @@
-export function SearchSectionInput() {
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+export function SearchSectionInput({ currentKeyword }) {
+  const [currentScrollIndex, setCurrentScrollIndex] = useState(0);
+
+  async function handleSearchClick() {
+    const tabs = await chrome.tabs.query({ currentWindow: true, active: true });
+
+    const activeTab = tabs[0];
+    const response = await chrome.tabs.sendMessage(activeTab.id, {
+      init: true,
+      keyword: currentKeyword,
+    });
+
+    if (response.isDone) {
+      setCurrentScrollIndex(0);
+    }
+  }
+
+  async function handleArrowClick(direction) {
+    const tabs = await chrome.tabs.query({ currentWindow: true, active: true });
+
+    const activeTab = tabs[0];
+    const response = await chrome.tabs.sendMessage(activeTab.id, {
+      step: direction === "next" ? 1 : -1,
+      currentScrollIndex,
+      keyword: currentKeyword,
+    });
+
+    if (response.isDone) {
+      if (direction === "next") {
+        setCurrentScrollIndex(currentScrollIndex + 1);
+      } else {
+        setCurrentScrollIndex(currentScrollIndex - 1);
+      }
+    }
+  }
+
   return (
     <>
       <input
@@ -10,9 +48,12 @@ export function SearchSectionInput() {
         <span className="text-[#ccc] font-extralight leading-[50px]">
           Total
         </span>
-        <button>↑</button>
-        <button>↓</button>
-        <button className="bg-[#333] w-[70px] h-full flex justify-center items-center rounded-r-full">
+        <button onClick={() => handleArrowClick("prev")}>↑</button>
+        <button onClick={() => handleArrowClick("next")}>↓</button>
+        <button
+          onClick={() => handleSearchClick()}
+          className="bg-[#333] w-[70px] h-full flex justify-center items-center rounded-r-full"
+        >
           <img
             src="./search_icon.png"
             alt="search_icon"
@@ -23,3 +64,7 @@ export function SearchSectionInput() {
     </>
   );
 }
+
+SearchSectionInput.propTypes = {
+  currentKeyword: PropTypes.string.isRequired,
+};
