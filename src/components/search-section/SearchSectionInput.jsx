1b49ab05ab373fad8845a02b98890b2befd5171b
hongya <hong7ya@gmail.com>
2025-01-22 23:08:25 +0900

    feat: search section 내 search 기능 구현 (#36)
    * feat: 구글 검색 페이지에 있을 시 기본적으로 하이라이팅
    
    * feat: 사이드패널 input 초기값은 여러 키워드 중 첫 번째 키워드로 설정
    
    * feat: 동일한 키워드 갯수를 표시
    
    * fix: 스크롤 시 이전 요소의 배경색이 사라지지 않게 수정
    
    * fix: indicator가 필요할 때 표시
    
    * feat: 엔터 또는 돋보기 버튼 클릭시 keywordGroup 내부에 키워드 추가
    
    * feat: keywordGroup에 추가와 동시에 하이라이팅
    
    * fix: 하이라이트 중복 적용되지 않도록 수정
    
    * fix: 스크롤과 input과 indicator와 keywordGroup간의 동기화

--- src/components/search-section/SearchSectionInput.jsx
+++ src/components/search-section/SearchSectionInput.jsx
@@ -1,39 +1,41 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
 
-export function SearchSectionInput({ currentKeyword }) {
-  const [currentScrollIndex, setCurrentScrollIndex] = useState(0);
+function Indicator({ currentScrollIndex, countsPerKeywords }) {
+  return (
+    <span className="text-[#ccc] font-extralight leading-[50px]">
+      {`${currentScrollIndex === -1 ? 0 : currentScrollIndex + 1}/${countsPerKeywords[0] ? countsPerKeywords[0].count : 0}`}
+    </span>
+  );
+}
 
-  async function handleSearchClick() {
-    const tabs = await chrome.tabs.query({ currentWindow: true, active: true });
-
-    const activeTab = tabs[0];
-    const response = await chrome.tabs.sendMessage(activeTab.id, {
-      init: true,
-      keyword: currentKeyword,
-    });
-
-    if (response.isDone) {
-      setCurrentScrollIndex(0);
-    }
-  }
+export function SearchSectionInput({ handleEnter, countsPerKeywords }) {
+  const [currentScrollIndex, setCurrentScrollIndex] = useState(-1);
+  const [value, setValue] = useState(
+    countsPerKeywords[0] ? countsPerKeywords[0].keyword : ""
+  );
+  const [isShowing, setIsShowing] = useState(
+    countsPerKeywords[0] ? true : false
+  );
 
   async function handleArrowClick(direction) {
+    const existingKeywords = countsPerKeywords.map(({ keyword }) => keyword);
+
+    if (!existingKeywords.includes(value)) {
+      return;
+    }
+
     const tabs = await chrome.tabs.query({ currentWindow: true, active: true });
 
     const activeTab = tabs[0];
     const response = await chrome.tabs.sendMessage(activeTab.id, {
       step: direction === "next" ? 1 : -1,
       currentScrollIndex,
-      keyword: currentKeyword,
+      keyword: value,
     });
 
     if (response.isDone) {
-      if (direction === "next") {
-        setCurrentScrollIndex(currentScrollIndex + 1);
-      } else {
-        setCurrentScrollIndex(currentScrollIndex - 1);
-      }
+      setCurrentScrollIndex(response.newIndex);
     }
   }
 
@@ -43,15 +45,40 @@ export function SearchSectionInput({ currentKeyword }) {
         type="text"
         className="w-full h-[50px] border border-[100] pl-[20px] my-[30px] text-[#333] rounded-full"
         placeholder="키워드를 입력해 주세요"
+        value={value}
+        onChange={(event) => {
+          setIsShowing(false);
+          setValue(event.target.value);
+        }}
+        onKeyDown={(event) => {
+          const existingKeywords = countsPerKeywords.map(
+            ({ keyword }) => keyword
+          );
+
+          if (event.key === "Enter" && !existingKeywords.includes(value)) {
+            handleEnter(value);
+          }
+        }}
       />
       <div className="buttonWrap absolute top-[75px] right-0 h-[50px] flex gap-[15px]">
-        <span className="text-[#ccc] font-extralight leading-[50px]">
-          Total
-        </span>
+        {isShowing && (
+          <Indicator
+            currentScrollIndex={currentScrollIndex}
+            countsPerKeywords={countsPerKeywords}
+          />
+        )}
         <button onClick={() => handleArrowClick("prev")}>↑</button>
         <button onClick={() => handleArrowClick("next")}>↓</button>
         <button
-          onClick={() => handleSearchClick()}
+          onClick={() => {
+            const existingKeywords = countsPerKeywords.map(
+              ({ keyword }) => keyword
+            );
+
+            if (!existingKeywords.includes(value)) {
+              handleEnter(value);
+            }
+          }}
           className="bg-[#333] w-[70px] h-full flex justify-center items-center rounded-r-full"
         >
           <img
@@ -65,6 +92,12 @@ export function SearchSectionInput({ currentKeyword }) {
   );
 }
 
+Indicator.propTypes = {
+  currentScrollIndex: PropTypes.number.isRequired,
+  countsPerKeywords: PropTypes.array.isRequired,
+};
+
 SearchSectionInput.propTypes = {
-  currentKeyword: PropTypes.string.isRequired,
+  handleEnter: PropTypes.func.isRequired,
+  countsPerKeywords: PropTypes.array.isRequired,
 };
--- src/components/search-section/index.jsx
+++ src/components/search-section/index.jsx
@@ -1,52 +1,106 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { convertToLinkMap } from "../../../background/convertToLinkMap";
 import TextButton from "../shared/TextButton";
-import { KeywordGroup } from "./KetwordGroup";
+import { KeywordGroup } from "./KeywordGroup";
 import { SearchSectionInput } from "./SearchSectionInput";
 
 export default function SearchSection() {
   const [isKeywordOn, setIsKeywordOn] = useState(false);
-  const [keywords, setKeywords] = useState([]);
-  const [currentKeyword, setCurrentKeyword] = useState("");
+  const [keywordsForSearch, setKeywordsForSearch] = useState([]);
+  const [bonus, setBonus] = useState([]);
 
-  async function handleKeywordClick() {
-    setIsKeywordOn(!isKeywordOn);
-    if (!isKeywordOn) {
-      const tabIdToLinkMapJson = await chrome.storage.local.get(null);
-      const linkMapJsons = Object.values(tabIdToLinkMapJson);
-      const linkMap = convertToLinkMap(linkMapJsons[0]);
-      const mapIterator = linkMap.values();
-      for (const { keywords } of mapIterator) {
-        setKeywords(keywords);
-        setCurrentKeyword(keywords[0]);
+  const [countsPerKeywords, setCountsPerKeywords] = useState([]);
 
-        break;
+  async function collectAllLinkMap() {
+    const tabIdToLinkMapJson = await chrome.storage.local.get(null);
+    const AllLinkMapJsons = Object.values(tabIdToLinkMapJson);
+
+    const allInOneLinkMap = new Map();
+
+    AllLinkMapJsons.forEach((linkMapJson) => {
+      const linkMap = convertToLinkMap(linkMapJson);
+      for (const [link, { description, keywords }] of linkMap) {
+        allInOneLinkMap.set(link, { description, keywords });
       }
-    }
+    });
+
+    return allInOneLinkMap;
   }
 
-  if (keywords.length > 0) {
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      const activeTab = tabs[0];
-      chrome.tabs.sendMessage(activeTab.id, { keywords });
-    });
+  async function getActiveTab() {
+    const tabs = await chrome.tabs.query({ currentWindow: true, active: true });
+
+    const activeTab = tabs[0];
+
+    return activeTab;
+  }
+
+  useEffect(() => {
+    async function makeCountsPerKeywords(group) {
+      if (group.length > 0) {
+        const tabs = await chrome.tabs.query({
+          currentWindow: true,
+          active: true,
+        });
+        const activeTab = tabs[0];
+        const response = await chrome.tabs.sendMessage(activeTab.id, {
+          keywords: group,
+        });
+
+        setCountsPerKeywords(response);
+      }
+    }
+
+    makeCountsPerKeywords([...bonus, ...keywordsForSearch]);
+  }, [bonus, keywordsForSearch]);
+
+  async function fireSearch() {
+    const allInOneLinkMap = await collectAllLinkMap();
+    const detectedKeywordsByGoogle = [];
+
+    const { url } = await getActiveTab();
+
+    const link = url.includes("#:~:text=")
+      ? url.substring(0, url.indexOf("#:~:text="))
+      : url;
+
+    if (allInOneLinkMap.has(link)) {
+      const { keywords } = allInOneLinkMap.get(link);
+      detectedKeywordsByGoogle.push(...keywords);
+    }
+
+    return detectedKeywordsByGoogle;
+  }
+
+  async function handleKeywordTextButtonClick(isKeywordOn) {
+    const nextIsKeywordOn = !isKeywordOn;
+
+    setIsKeywordOn(nextIsKeywordOn);
+
+    if (nextIsKeywordOn) {
+      const detectedKeywordsByGoogle = await fireSearch();
+      setKeywordsForSearch([...detectedKeywordsByGoogle]);
+    }
   }
 
   return (
     <>
-      <SearchSectionInput currentKeyword={currentKeyword} />
+      <SearchSectionInput
+        key={countsPerKeywords[0] ? countsPerKeywords[0].keyword : "default"}
+        handleEnter={(value) => {
+          setBonus((prv) => [value, ...prv]);
+        }}
+        countsPerKeywords={countsPerKeywords}
+      />
       <div className="flex gap-[15px]">
         <TextButton text={"Descrition"} />
         <TextButton
           text={"keyword"}
-          onClick={() => handleKeywordClick()}
+          onClick={() => handleKeywordTextButtonClick(isKeywordOn)}
         />
       </div>
-      <KeywordGroup
-        currentKeyword={currentKeyword}
-        keywords={keywords}
-      />
+      <KeywordGroup countsPerKeywords={countsPerKeywords} />
     </>
   );
 }
