52a0bceda8a7a1dbdaec9bebbf4c324652fb2f29
JSLee <mac@macui-MacBookPro.local>
2025-01-23 03:29:38 +0900

    feat: history item 클릭 시 해당 url 이동

--- src/components/lately-history-croup/LatelyHistoryGroup.jsx
+++ src/components/lately-history-croup/LatelyHistoryGroup.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 import { getHistories } from "../../firebase/CRUD";
+import HistoryItem from "../shared/HistoryItem";
 
 export default function LatelyHistoryGroup() {
   const [historyList, setHistoryList] = useState([]);
@@ -28,15 +29,12 @@ export default function LatelyHistoryGroup() {
       <ul className="bg-[#f6f6f6] h-60 overflow-y-scroll border text-left px-[10px] py-[20px]">
         {historyList.map((history, index) => {
           return (
-            <li
+            <HistoryItem
               key={index}
-              className="text-xs text-[#555] bg-[#fff] border px-[15px] mb-[15px] py-[10px] rounded-full w-full"
-            >
-              <a href={history.url}>
-                {history.faviconSrc}
-                {history.siteTitle}
-              </a>
-            </li>
+              url={history.url}
+              favicon={history.faviconSrc}
+              siteTitle={history.siteTitle}
+            />
           );
         })}
       </ul>
