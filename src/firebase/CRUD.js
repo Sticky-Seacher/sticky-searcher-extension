71e1059d2084c2facd9fd46564686ccb3583fc66
JSLee <mac@macui-MacBookPro.local>
2025-01-23 04:54:19 +0900

    feat: history item 최대한도 10개 지정

--- src/firebase/CRUD.js
+++ src/firebase/CRUD.js
@@ -3,6 +3,7 @@ import {
   deleteDoc,
   doc,
   getDocs,
+  limit,
   query,
   setDoc,
 } from "firebase/firestore";
@@ -55,7 +56,10 @@ async function getGroups(token, callback) {
 }
 
 async function getHistories(token, groupId, callback) {
-  const histories = query(collection(db, token, groupId, "histories"));
+  const histories = query(
+    collection(db, token, groupId, "histories"),
+    limit(10)
+  );
   const historyList = await getDocs(histories);
   historyList.forEach((history) => {
     const historyData = {
