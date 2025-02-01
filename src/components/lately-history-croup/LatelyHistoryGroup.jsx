cf29f78db91bab54e9787823d602bd2e6c8c936d
JSLee <mac@macui-MacBookPro.local>
2025-02-01 18:00:40 +0900

    feat: history item 노출

--- src/components/lately-history-croup/LatelyHistoryGroup.jsx
+++ src/components/lately-history-croup/LatelyHistoryGroup.jsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react";
+
+import { useUserInfo } from "../../context/UserInfo";
+import { getHistoriesInDefaultGroup } from "../../firebase/history";
+import { getUser } from "../../firebase/user";
+import HistoryItem from "../shared/HistoryItem";
+
 export default function LatelyHistoryGroup() {
+  const [historyItem, setHistoryItem] = useState([]);
+  const { userInfo } = useUserInfo();
+
+  useEffect(() => {
+    async function getHistoryItem(userEmail) {
+      let userId = await getUser(userEmail);
+      const histories = await getHistoriesInDefaultGroup(userId);
+      setHistoryItem(histories);
+    }
+    userInfo[0] && getHistoryItem(userInfo[0]);
+  }, [userInfo]);
+
   return (
     <div>
       <p className="text-lg font-semibold mt-[10px] mb-[10px]">
         lately History Group
       </p>
-      <ul className="bg-[#f6f6f6] h-60 overflow-y-scroll border text-left px-[10px] py-[20px]"></ul>
+      <ul className="bg-[#f6f6f6] h-60 overflow-y-scroll border text-left px-[10px] py-[20px]">
+        {historyItem.map((history, index) => {
+          return (
+            <HistoryItem
+              key={index}
+              favicon={history.faviconSrc}
+              siteTitle={history.siteTitle}
+              url={history.url}
+            />
+          );
+        })}
+      </ul>
     </div>
   );
 }
--- src/components/shared/HistoryItem.jsx
+++ src/components/shared/HistoryItem.jsx
@@ -1,5 +1,26 @@
-export default function HistoryItem() {
+import PropTypes from "prop-types";
+
+export default function HistoryItem({ favicon, siteTitle, url }) {
+  function handleMoveUrl() {
+    chrome.tabs.create({ url: url });
+  }
   return (
-    <li className="text-xs text-[#555] bg-[#fff] border px-[15px] mb-[15px] py-[10px] rounded-full w-full"></li>
+    <li className="text-xs text-[#555] bg-[#fff] border px-[15px] mb-[15px] py-[10px] rounded-full w-full">
+      <div
+        onClick={handleMoveUrl}
+        className="flex gap-[10px] justify-start items-center"
+      >
+        <div className="w-[20px] h-[20px] object-fill rounded-full">
+          <img src={favicon} />
+        </div>
+        {siteTitle}
+      </div>
+    </li>
   );
 }
+
+HistoryItem.propTypes = {
+  url: PropTypes.string.isRequired,
+  favicon: PropTypes.string.isRequired,
+  siteTitle: PropTypes.string.isRequired,
+};
