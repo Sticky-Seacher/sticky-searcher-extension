06d1a9be9ebdbc7d164488451ab55f0e7c7ed1d8
YeonJu <mpnisck@gmail.com>
2025-04-12 14:24:07 +0900

    fix: 블루아르곤 에러 개선 및 버전 업데이트 (#60)

--- src/components/shared/TextButton.jsx
+++ src/components/shared/TextButton.jsx
@@ -4,7 +4,7 @@ export default function TextButton({ text, onClick }) {
   return (
     <button
       onClick={onClick}
-      className="bg-[#333] text-[#fff] border w-[50%] py-[10px] rounded-full"
+      className="bg-[#333] text-[#fff] border w-[50%] px-[20px] py-[10px] rounded-full"
     >
       {text}
     </button>
