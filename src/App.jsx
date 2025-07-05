9d9b5bf801757def152e067991f346769e32caa0
hongya <hong7ya@gmail.com>
2025-07-05 17:49:54 +0900

    chore: 개발을 위해 설치했던 ReactQueryDevtools 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Suspense, lazy, useEffect, useState } from "react";
+import { useState } from "react";
 
 import HistorySection from "./components/history-section";
 import LatelyHistoryGroup from "./components/lately-history-croup/LatelyHistoryGroup";
@@ -9,22 +8,8 @@ import UserInfoProvider from "./context/UserInfo";
 
 const queryClient = new QueryClient();
 
-const ReactQueryDevtoolsProduction = lazy(() =>
-  import("@tanstack/react-query-devtools/build/modern/production.js").then(
-    (d) => ({
-      default: d.ReactQueryDevtools,
-    })
-  )
-);
-
 function App() {
   const [countsPerKeywords, setCountsPerKeywords] = useState([]);
-
-  const [showDevtools, setShowDevtools] = useState(false);
-
-  useEffect(() => {
-    window.toggleDevtools = () => setShowDevtools((old) => !old);
-  }, []);
 
   chrome.runtime.onMessage.addListener((request) => {
     if (request.message === "Get user authentication") {
@@ -44,13 +29,6 @@ function App() {
           setCountsPerKeywords={setCountsPerKeywords}
         />
         <LatelyHistoryGroup />
-        {/* [빌드된 파일에서 ReactQueryDevtools 사용] 사이트 패널의 콘솔에서 window.toggleDevtools() 입력시 사용 가능 */}
-        <ReactQueryDevtools initialIsOpen />
-        {showDevtools && (
-          <Suspense fallback={null}>
-            <ReactQueryDevtoolsProduction />
-          </Suspense>
-        )}
       </QueryClientProvider>
     </UserInfoProvider>
   );
